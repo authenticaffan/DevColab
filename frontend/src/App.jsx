6d3351fae9ed688cb0935b54e449db51113331ed
Mohamed Affan <mohamedaffan477@gmail.com>
2024-07-06 10:18:21 +0000

    initial setup and basic components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blog from './components/Blog/Blog'
+import Community from './components/Community/Community'
+import Hero from './components/Hero/Hero'
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Community />
+      <Blog />
+    </>
+  )
+}
+
+export default App
